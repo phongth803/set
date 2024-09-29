0d1c9b2b25b9a08cd0e2be748a32b5a66a404a90
Tran Hoai Phong <thp080203@mail.com>
2024-09-29 14:12:41 +0700

    Add ThinkingInReact project files and structure

---
+++ ThinkingInReact/src/components/FilterableProductTable/FilterableProductTable.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+import ProductTable from '../ProductTable/ProductTable';
+
+function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState('');
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  return (
+    <div>
+      <SearchBar 
+        filterText={filterText} 
+        inStockOnly={inStockOnly} 
+        onFilterTextChange={setFilterText} 
+        onInStockOnlyChange={setInStockOnly} />
+      <ProductTable 
+        products={products} 
+        filterText={filterText}
+        inStockOnly={inStockOnly} />
+    </div>
+  );
+}
+
+export default FilterableProductTable;
